aeeb1c45eda1f88b69d4fa70791391a2d72389b0
Jonny Haldas <jonathan.haldas@gmail.com>
2024-02-09 16:06:05 -0500

    Update Script

--- Viewtron-XML-Parser.py
+++ Viewtron-XML-Parser.py
@@ -7,23 +7,22 @@ app = Flask(__name__)
 
 @app.route("/alarm", methods=['GET', 'POST'])
 def getInfo():
+    smartTypeText = ""
     print("Request Type", request.method)
     if(request.method != "GET"):
         textInput = request.data
         ### print(textInput)
         root = ET.fromstring(textInput)
 
-        for child in root:
-            print("'" + child.tag + "'", child.attrib, child.text)
-
-        print("=======================")
-
-        for child in root:
-            if child.tag == 'smartType':
+        for child in root.findall("{http://www.ipc.com/ver10}smartType"):
                 smartTypeText = child.text
                 print(smartTypeText)
-                break
-        
+            
+        if smartTypeText == "PEA":
+            print("Do Something")
+        else:
+            print("Do Nothing")
+            
 
     return "<p>BLANK</p>"
 
