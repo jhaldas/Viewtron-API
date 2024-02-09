5e995d4e23feb19751bb88f806c118fa4a5b94ca
Jonny Haldas <jonathan.haldas@gmail.com>
2024-02-09 17:01:18 -0500

    Update Script

--- Viewtron-XML-Parser.py
+++ Viewtron-XML-Parser.py
@@ -1,5 +1,7 @@
 from flask import Flask, request, render_template, jsonify
 import xml.etree.ElementTree as ET
+import requests
+import json
 
 
 app = Flask(__name__)
@@ -20,11 +22,18 @@ def getInfo():
             
         if smartTypeText == "PEA":
             print("Do Something")
+            DoSomething(smartTypeText)
         else:
             print("Do Nothing")
+            DoSomething("Test")
             
 
     return "<p>BLANK</p>"
+
+def DoSomething(text):
+    webhook_url = 'https://webhook.site/3ba84f14-2eb2-441f-ae8f-f2c8c2783a40'
+    data = { 'smartType': text }
+    r = requests.post(webhook_url, data = json.dumps(data), headers = {'Content-Type':'application/json'})
 
     
 @app.route("/test")
