6e5379331d58b7f9a160257a82ef45263f1c240a
Jonny Haldas <jonathan.haldas@gmail.com>
2024-02-20 12:30:12 -0500

    Update Viewtron-XML-Parser.py

--- Viewtron-XML-Parser.py
+++ Viewtron-XML-Parser.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 @app.route("/alarm", methods=['GET', 'POST'])
 def getInfo():
     smartTypeText = ""
-    print("Request Type", request.method)
+    #print("Request Type", request.method)
     if(request.method != "GET"):
         textInput = request.data
         ### print(textInput)
@@ -18,20 +18,22 @@ def getInfo():
 
         for child in root.findall("{http://www.ipc.com/ver10}smartType"):
                 smartTypeText = child.text
-                print(smartTypeText)
+                print("\n")
+                print("INTRUSION ALERT:", smartTypeText)
+                print("\n")
             
-        if smartTypeText == "PEA":
-            print("Do Something")
-            DoSomething(smartTypeText)
-        else:
-            print("Do Nothing")
-            DoSomething("Test")
+       #if smartTypeText == "PEA":
+            #DoSomething(smartTypeText)
+
+    else:
+        print("Do Nothing")
+        #DoSomething("Test")
             
 
     return "<p>BLANK</p>"
 
 def DoSomething(text):
-    webhook_url = 'https://webhook.site/3ba84f14-2eb2-441f-ae8f-f2c8c2783a40'
+    webhook_url = 'https://webhook.site/b498f8f8-e68b-41a2-8d43-6dedef3f4f60'
     data = { 'smartType': text }
     r = requests.post(webhook_url, data = json.dumps(data), headers = {'Content-Type':'application/json'})
 
